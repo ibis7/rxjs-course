b6a06e7fd4ebb93e09c1ec823e79dca9e65ee380
Marco Detta <marco.detta@endava.com>
2025-07-17 17:34:17 -0300

    flatmap vs switchmap

--- flatMap_vs_switchMap.js
+++ flatMap_vs_switchMap.js
@@ -1,25 +1,23 @@
-let {Observable} = require('rxjs/Observable');
-require('rxjs/add/observable/interval');
-require('rxjs/add/operator/mergeMap'); // flatMap is an alias of mergeMap
-require('rxjs/add/operator/switchMap');
-require('rxjs/add/operator/take');
-require('rxjs/add/operator/map');
+import { interval } from "rxjs";
+import { map, mergeMap, take } from "rxjs/operators";
 
-let outer = Observable.interval(1000).take(2);
+let outer = interval(1000).pipe(take(2));
 
-let combined = outer.flatMap((x) => {
-    return Observable
-        .interval(400)
-        .take(3)
-        .map(y => `outer ${x}: inner ${y}`)
-});
+let combined = outer.pipe(
+  mergeMap((x) => {
+    return interval(400).pipe(
+      take(3),
+      map((y) => `outer ${x}: inner ${y}`)
+    );
+  })
+);
 
-combined.subscribe(result => console.log(`result ${result} `));
+combined.subscribe((result) => console.log(`result ${result} `));
 
 /*
-* Although the third value of the inner observable is emmited after the outer observable emits new value,
-* all emissions of the inner observable are present in the merged output.
-*
-* Change flatMap to switchMap and the third value of the inner observable is not emitted because
-* the outer observable emitted new value and the inner one starts handling it.
-* */
+ * Although the third value of the inner observable is emmited after the outer observable emits new value,
+ * all emissions of the inner observable are present in the merged output.
+ *
+ * Change flatMap to switchMap and the third value of the inner observable is not emitted because
+ * the outer observable emitted new value and the inner one starts handling it.
+ * */
