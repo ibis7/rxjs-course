c80aeb81a2772b69e8068b5f8dc7384d45041b3a
Marco Detta <marco.detta@kineticadvantage.com>
2025-07-17 17:04:36 -0300

    some changes

--- concat.js
+++ concat.js
@@ -1,14 +1,8 @@
-let {Observable} = require('rxjs/Observable');
-require('rxjs/add/observable/concat');
-require('rxjs/add/observable/timer');
-require('rxjs/add/operator/mapTo');
+import { concat, map, timer } from "rxjs";
 
-// Emulate first HTTP request that take 3 sec
-let threeSecHTTPRequest = Observable.timer(3000).mapTo('First response');
+const threeSecHTTPRequest = timer(3000).pipe(map(() => "First response"));
+const oneSecHTTPRequest = timer(1000).pipe(map(() => "Second response"));
 
-// Emulate second HTTP request that takes 1 sec
-let oneSecHTTPRequest = Observable.timer(1000).mapTo('Second response');
-
-Observable
-    .concat(threeSecHTTPRequest, oneSecHTTPRequest)
-    .subscribe(res => console.log(res));
+concat(threeSecHTTPRequest, oneSecHTTPRequest).subscribe((res) =>
+  console.log(res)
+);
